d6a3e74f3c1547122fe23387be8f08d48c9d0248
azmiOzyesil <azmiozyesil@gmail.com>
2022-10-18 19:52:43 +0300

    Week3 SQL Homework

---
+++ week3SqlHomework/SQLQuery2.sql
@@ -0,0 +1,3 @@
+﻿select *from Products p inner join [Order Details] od on p.ProductID = od.ProductID inner join Orders o on o.OrderID = od.OrderID
+
+select *from Customers c left join Orders o on c.CustomerID = o.CustomerID where o.CustomerID is null
